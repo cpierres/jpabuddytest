6e999588b58fde12a0ffb12c68569198d13b18f0
cpierres <cpierres@hotmail.com>
2023-12-11 16:10:14 +0100

    Initial commit - jpa spring project

---
+++ src/main/java/com/cpi/jpabuddytest/JpabuddytestApplication.java
@@ -0,0 +1,13 @@
+package com.cpi.jpabuddytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpabuddytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpabuddytestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpi/jpabuddytest/JpabuddytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpi.jpabuddytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpabuddytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
